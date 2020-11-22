accfe37a68b971a38038f0556032b4905e149943
= <=>
2020-11-22 17:34:27 -0300

    feat(operations): add sum and subfs functions.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,27 @@
-let firstNumber = document.querySelector('#firstNumber');
-let secondNumber = document.querySelector('#secondNumber');
+let firstInput = document.querySelector("#firstNumber");
+let secondInput = document.querySelector("#secondNumber");
+
+
+firstInput.addEventListener('input', () => {
+    start()
+})
+
+secondInput.addEventListener('input', () => {
+    start()
+})
+
+function sum() {
+    let resultSum = document.querySelector('#result-sum')
+    resultSum.value = parseInt(firstInput.value) + parseInt(secondInput.value)
+}
+
+function subfs() {
+    let resultSum = document.querySelector('#result-sub-fs')
+    resultSum.value = parseInt(firstInput.value) - parseInt(secondInput.value)
+}
+
+
+function start() {
+    sum()
+    subfs()
+}
